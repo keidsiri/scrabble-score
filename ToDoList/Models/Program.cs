5e0b885b7936440132edd018f1ed8546e6eb115d
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-09 11:33:26 -0800

    Initial commit

---
+++ ToDoList/Models/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;  //import namespaces from Item.cs
+
+class Program
+{
+  static void Main()
+  {
+    // program code goes here
+  }
+}
